d415f97c6e18fe7e915e7f1bd790ead925f84d5e
ryosuwito92 <ryo@coconutlab.io>
2025-08-12 00:01:33 +0700

    revise default height of map

--- google_maps_tool.py
+++ google_maps_tool.py
@@ -68,7 +68,7 @@ class Tools:
                 await __event_emitter__({
                     "type": "message", 
                     "data": {"content": f'''```html
-<iframe src="{data["embed_map_url"]}" width="100%" height="400" frameborder="0" allowfullscreen></iframe>
+<iframe src="{data["embed_map_url"]}" width="100%" height="720" frameborder="0" allowfullscreen></iframe>
 ```\n'''}
                 })
             return result
